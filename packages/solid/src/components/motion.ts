5d3e5866d2b880a9e175e7d081b2d3b2035970be
juan <47119916+jhomra21@users.noreply.github.com>
2026-08-23 14:47:11 -0500

    fix(motion): bind props during Solid render phase

--- packages/solid/src/components/motion.ts
+++ packages/solid/src/components/motion.ts
@@ -1,12 +1,16 @@
-import { createMemo } from "solid-js"
-import { createHostElement, type HostElementNode } from "../host/nodes.js"
+import { createRenderEffect } from "solid-js"
+import {
+  createHostElement,
+  setHostProperty,
+  type HostElementNode,
+} from "../host/nodes.js"
 import type {
   HostProps,
   HostRef,
   MotionProps,
   StyleDesc,
 } from "../host/types.js"
-import { spread } from "../host/universal.js"
+import { insert, spread } from "../host/universal.js"
 
 export interface MotionDivProps extends MotionProps {
   children?: unknown
@@ -27,19 +31,31 @@ export interface MotionDivProps extends MotionProps {
   autoFocus?: boolean
 }
 
-function MotionDiv(props: MotionDivProps): HostElementNode {
-  const node = createHostElement("div")
-  const nativeMotion = createMemo<MotionProps>(() => {
-    const value: MotionProps = { animate: props.animate }
-    if (props.initial !== undefined) value.initial = props.initial
-    if (props.transition !== undefined) value.transition = props.transition
-    return value
-  })
+interface MotionHostSnapshot {
+  style: StyleDesc | undefined
+  onClick: HostProps["onClick"]
+  onMouseDown: HostProps["onMouseDown"]
+  onMouseUp: HostProps["onMouseUp"]
+  onMouseEnter: HostProps["onMouseEnter"]
+  onMouseLeave: HostProps["onMouseLeave"]
+  onMouseMove: HostProps["onMouseMove"]
+  onMouseDownOutside: HostProps["onMouseDownOutside"]
+  onKeyDown: HostProps["onKeyDown"]
+  onKeyUp: HostProps["onKeyUp"]
+  onFocus: HostProps["onFocus"]
+  onBlur: HostProps["onBlur"]
+  onScroll: HostProps["onScroll"]
+  autoFocus: boolean | undefined
+  motion: MotionProps
+}
 
-  spread(node, () => ({
-    children: props.children,
+function readMotionProps(props: MotionDivProps): MotionHostSnapshot {
+  const motion: MotionProps = { animate: props.animate }
+  if (props.initial !== undefined) motion.initial = props.initial
+  if (props.transition !== undefined) motion.transition = props.transition
+
+  return {
     style: props.style,
-    ref: props.ref,
     onClick: props.onClick,
     onMouseDown: props.onMouseDown,
     onMouseUp: props.onMouseUp,
@@ -53,9 +69,51 @@ function MotionDiv(props: MotionDivProps): HostElementNode {
     onBlur: props.onBlur,
     onScroll: props.onScroll,
     autoFocus: props.autoFocus,
-    motion: nativeMotion(),
-  }))
+    motion,
+  }
+}
 
+function applyMotionProps(
+  node: HostElementNode,
+  next: MotionHostSnapshot,
+  previous: MotionHostSnapshot | undefined,
+): void {
+  setHostProperty(node, "style", next.style, previous?.style)
+  setHostProperty(node, "onClick", next.onClick, previous?.onClick)
+  setHostProperty(node, "onMouseDown", next.onMouseDown, previous?.onMouseDown)
+  setHostProperty(node, "onMouseUp", next.onMouseUp, previous?.onMouseUp)
+  setHostProperty(node, "onMouseEnter", next.onMouseEnter, previous?.onMouseEnter)
+  setHostProperty(node, "onMouseLeave", next.onMouseLeave, previous?.onMouseLeave)
+  setHostProperty(node, "onMouseMove", next.onMouseMove, previous?.onMouseMove)
+  setHostProperty(
+    node,
+    "onMouseDownOutside",
+    next.onMouseDownOutside,
+    previous?.onMouseDownOutside,
+  )
+  setHostProperty(node, "onKeyDown", next.onKeyDown, previous?.onKeyDown)
+  setHostProperty(node, "onKeyUp", next.onKeyUp, previous?.onKeyUp)
+  setHostProperty(node, "onFocus", next.onFocus, previous?.onFocus)
+  setHostProperty(node, "onBlur", next.onBlur, previous?.onBlur)
+  setHostProperty(node, "onScroll", next.onScroll, previous?.onScroll)
+  setHostProperty(node, "autoFocus", next.autoFocus, previous?.autoFocus)
+  setHostProperty(node, "motion", next.motion, previous?.motion)
+}
+
+function MotionDiv(props: MotionDivProps): HostElementNode {
+  const node = createHostElement("div")
+  let previous: MotionHostSnapshot | undefined
+
+  createRenderEffect(
+    () => readMotionProps(props),
+    (next) => {
+      applyMotionProps(node, next, previous)
+      previous = next
+    },
+  )
+
+  if (props.ref) spread(node, { ref: props.ref }, true)
+  insert(node, () => props.children)
   return node
 }
 
