cf9d76030c94c90fb8ee5b7d068413235516d7ef
juan <47119916+jhomra21@users.noreply.github.com>
2026-09-06 10:35:21 -0500

    fix: type Solid 1 runtime rejection references

--- packages/solid1/src/runtime.ts
+++ packages/solid1/src/runtime.ts
@@ -27,7 +27,10 @@ type RenderSlot = {
 }
 
 type RuntimeFailure = Error | string
-type RuntimeRejectionReason = Error | string | number | boolean | bigint | symbol | object | null | undefined
+interface RuntimeRejectionReference {
+  toString(): string
+}
+type RuntimeRejectionReason = Error | string | number | boolean | bigint | symbol | RuntimeRejectionReference | null | undefined
 
 type RuntimeErrorHandlers = {
   uncaughtException: (error: Error) => void
