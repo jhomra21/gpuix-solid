17c49a688c3428d34b6b6a02ff6a5de8ac8b85eb
juan <47119916+jhomra21@users.noreply.github.com>
2026-09-14 14:33:52 -0500

    examples: add GPUIX 0.8 showcase entry

---
+++ examples/counter/src/gpuix-08/index.tsx
@@ -0,0 +1,8 @@
+import { render } from "gpuix-solid"
+import { Gpuix08Showcase } from "./app"
+
+render(() => <Gpuix08Showcase />, {
+  title: "GPUix Solid · GPUIX 0.8",
+  width: 860,
+  height: 720,
+})
