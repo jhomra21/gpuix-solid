226567309bab7f77ec5a0fe7ca580de2f81c7e09
juan <47119916+jhomra21@users.noreply.github.com>
2026-09-19 19:19:21 -0500

    feat(canvas): export Solid 1 Canvas API

--- packages/solid1/src/index.ts
+++ packages/solid1/src/index.ts
@@ -66,6 +66,7 @@ export {
 
 export type {
   AnchoredProps,
+  CanvasProps,
   CodeProps,
   CursorValue,
   DebugFrameOverlayMode,
@@ -99,6 +100,17 @@ export type {
   WindowKeyEventHandler,
   WindowKeyEventHandlers,
 } from "./host/types.js"
+
+export { CANVAS_DRAW_LIST_VERSION, createCanvas2DRecorder } from "./host/canvas.js"
+export type {
+  Canvas2DRecorder,
+  CanvasBackingSize,
+  CanvasDrawCommand,
+  CanvasDrawList,
+  CanvasDrawListVersion,
+  CanvasMatrix,
+  CanvasPathSegment,
+} from "./host/canvas.js"
 
 export { GpuixRenderer } from "@gpuix/native"
 export type {
